5d0813585917b159b457d2e4897ef1ae39ee06c6
Ethan Pippin <ethanpippin2343@gmail.com>
2019-01-21 21:46:13 -0700

    Remove Firebase

--- candidates/views.py
+++ candidates/views.py
@@ -9,57 +9,60 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from Firebase import Firebase
 from .models import Candidate
 
 def candidates_list(request):
-    template = 'candidates_list.html'
-    candidates = Firebase.instance().get_candidates()
-    context = {'candidates': candidates}
-    return render(request, template, context)
+    # template = 'candidates_list.html'
+    # candidates = Firebase.instance().get_candidates()
+    # context = {'candidates': candidates}
+    # return render(request, template, context)
+    return HttpResponse("This isn't finished yet")
 
 def view_candidate(request, id):
     # TODO
     return
 
 def edit_candidate(request, id):
-    template = 'edit_candidate.html'
-    candidate = Firebase.instance().get_candidate(id)
-    context = {'candidate': candidate}
-    return render(request, template, context)
+    # template = 'edit_candidate.html'
+    # candidate = Firebase.instance().get_candidate(id)
+    # context = {'candidate': candidate}
+    # return render(request, template, context)
+    return HttpResponse("This isn't finished yet")
 
 def set_candidate(request, id):
-    first = request.POST['first_name']
-    last = request.POST['last_name']
-    image_url = request.POST['image_url']
-    if len(image_url) < 5:
-        image_url = None # Hack to get null
-
-    sections = []
-    for key in request.POST:
-        if key.startswith("section_content_"):
-            section = {}
-            section["title"] = key[16:] # Hack
-            section["content"] = request.POST[key]
-            sections.append(section)
-    candidate = Candidate(first=first,
-                          last=last,
-                          id=id,
-                          image_url=image_url,
-                          sections=sections)
-    Firebase.instance().set_candidate(candidate)
-    return redirect("/candidates")
+    # first = request.POST['first_name']
+    # last = request.POST['last_name']
+    # image_url = request.POST['image_url']
+    # if len(image_url) < 5:
+    #     image_url = None # Hack to get null
+    #
+    # sections = []
+    # for key in request.POST:
+    #     if key.startswith("section_content_"):
+    #         section = {}
+    #         section["title"] = key[16:] # Hack
+    #         section["content"] = request.POST[key]
+    #         sections.append(section)
+    # candidate = Candidate(first=first,
+    #                       last=last,
+    #                       id=id,
+    #                       image_url=image_url,
+    #                       sections=sections)
+    # Firebase.instance().set_candidate(candidate)
+    # return redirect("/candidates")
+    return HttpResponse("This isn't finished yet")
 
 def create_candidate(request):
-    template = 'edit_candidate.html'
-    id = id_generator()
-    candidate = Candidate(first=None,
-                          last=None,
-                          id=id,
-                          image_url=None,
-                          sections=None)
-    context = {'candidate': candidate}
-    return render(request, template, context)
+    # template = 'edit_candidate.html'
+    # id = id_generator()
+    # candidate = Candidate(first=None,
+    #                       last=None,
+    #                       id=id,
+    #                       image_url=None,
+    #                       sections=None)
+    # context = {'candidate': candidate}
+    # return render(request, template, context)
+    return HttpResponse("This isn't finished yet")
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
--- elections/views.py
+++ elections/views.py
@@ -3,61 +3,61 @@ import random
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from Firebase import Firebase
 from .models import Election
 
-
 def elections_list(request):
-    template = 'elections_list.html'
-    elections = Firebase.instance().get_elections()
-    context = {'elections': elections}
-    return render(request, template, context)
+    # template = 'elections_list.html'
+    # elections = Firebase.instance().get_elections()
+    # context = {'elections': elections}
+    # return render(request, template, context)
+    return HttpResponse("This isn't finished yet")
 
 def view_election(request):
     # TODO
     return
 
 def edit_election(request, id):
-    template = 'edit_election.html'
-    election = Firebase.instance().get_election(id)
-    context = {'election': election}
-    return render(request, template, context)
+    # template = 'edit_election.html'
+    # election = Firebase.instance().get_election(id)
+    # context = {'election': election}
+    # return render(request, template, context)
+    return HttpResponse("This isn't finished yet")
 
 def set_election(request, id):
-    title = request.POST['title']
-    category = request.POST['category']
-    cover_image_url = request.POST['cover_image_url']
-    if len(cover_image_url) < 5:
-        cover_image_url = None # Hack to get null
-
-    sections = []
-    for key in request.POST:
-        if key.startswith("section_content_"):
-            section = {}
-            section["title"] = key[16:] # Hack
-            section["content"] = request.POST[key]
-            sections.append(section)
-
-    election = Election(id=id,
-                        title=title,
-                        category=category,
-                        cover_image_url=cover_image_url,
-                        sections=sections)
-    Firebase.instance().set_election(election)
-    return redirect("/elections")
+    # title = request.POST['title']
+    # category = request.POST['category']
+    # cover_image_url = request.POST['cover_image_url']
+    # if len(cover_image_url) < 5:
+    #     cover_image_url = None # Hack to get null
+    #
+    # sections = []
+    # for key in request.POST:
+    #     if key.startswith("section_content_"):
+    #         section = {}
+    #         section["title"] = key[16:] # Hack
+    #         section["content"] = request.POST[key]
+    #         sections.append(section)
+    #
+    # election = Election(id=id,
+    #                     title=title,
+    #                     category=category,
+    #                     cover_image_url=cover_image_url,
+    #                     sections=sections)
+    # Firebase.instance().set_election(election)
+    # return redirect("/elections")
+    return HttpResponse("This isn't finished yet")
 
 def create_election(request):
-    template = 'edit_election.html'
-    id = id_generator()
-    election = Election(id=id,
-                        title=None,
-                        category=None,
-                        cover_image_url=None,
-                        sections=None)
-    context = {'election': election}
-    return render(request, template, context)
-
-
+    # template = 'edit_election.html'
+    # id = id_generator()
+    # election = Election(id=id,
+    #                     title=None,
+    #                     category=None,
+    #                     cover_image_url=None,
+    #                     sections=None)
+    # context = {'election': election}
+    # return render(request, template, context)
+    return HttpResponse("This isn't finished yet")
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
